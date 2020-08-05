e7d94dc956a0329c9421397bcff1267f34b6c21e
Himanshu <himanshusurana7@gmail.com>
2020-08-05 23:57:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get("/projects",function(req,res){
+	res.render("projects");
+});
+
+app.get("/",function(req,res){
+	//res.sendFile(__dirname + "/index.html");
+	res.render("home");
+});
+
+app.listen(3000,function(){
+	console.log("server started at port 3000");
+	});
